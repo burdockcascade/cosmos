f2379112f3fd1312ec999dd5a932cc48fe0a3901
Peter Richmond <3448039+burdockcascade@users.noreply.github.com>
2024-10-12 15:18:39 +0100

    chore: set optimization level

--- xmake.lua
+++ xmake.lua
@@ -1,12 +1,13 @@
 set_description("Cosmos Game Engine")
 set_version("0.1.0")
 
-set_languages("c++17")
+set_languages("cxx20")
 
 add_requires("raylib 5.0", "sol2 3.3.0")
 add_rules("mode.debug", "mode.release")
 
 target("cosmos")
+    set_optimize("fastest")
     add_packages("raylib",  "sol2")
     set_kind("binary")
     add_files("src/*.cpp")
